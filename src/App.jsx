b861ba9e4748f074f7467945047a35588785a80a
imshubham07 <mrshubhamdubey20@gmail.com>
2024-06-10 14:04:24 +0530

    WeatherApp Complete

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Temp from './Component/weather/temp'
+import Weathercard from './Component/weather/weathercard'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <Temp />
+    </div>
+    
+  )
+}
+
+export default App
